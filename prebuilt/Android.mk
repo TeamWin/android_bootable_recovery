e3a4983b0dcb98f11e0c33b0f731b29c57a06a8a
nailyk-fr <nailyk_git@nailyk.fr>
2017-08-08 21:23:35 +0200

    TWRP: add an option to exclude the TWRP app
     * Allow device maintainer to exclude the twrp app and disable
       the installation prompt. Mostly the app is useless for
       every unofficially supported devices.
    
     * BoardConfig flag: 'TW_EXCLUDE_TWRPAPP := true'
    
    Original commit: https://gerrit.omnirom.org/#/c/android_bootable_recovery/+/27694/
    
    Change-Id: I0aa1fb2ebe03ceb19c766178544039a82497cdd4

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -687,6 +687,7 @@ ifeq ($(TW_USE_TOOLBOX), true)
    include $(BUILD_PREBUILT)
 endif
 
+ifneq ($(TW_EXCLUDE_TWRPAPP),true)
 #TWRP App "placeholder"
 include $(CLEAR_VARS)
 LOCAL_MODULE := me.twrp.twrpapp.apk
@@ -695,6 +696,7 @@ LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+endif
 
 #TWRP App permissions for Android 9+
 include $(CLEAR_VARS)
