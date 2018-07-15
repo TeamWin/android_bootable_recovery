449cc3a7dbc0daf4ffbe07cccda29d761c4a76e6
Sean hoyt <seanhoyt963@gmail.com>
2018-07-15 06:16:09 +0200

    bootable: Change how blank screen works
    This is required on some devices where previous to 8.1/8.0 the blank
    screen flag worked but don't now.
    
    Test: Tested on begonia, screen is now no longer black
    
    Change-Id: Ib4ff607d220bcb1aa5166fea23cc7ecb0e012fdd
    (cherry picked from commit 28d8dec40d60a31309fcf6259dad38cd328f0717)

--- gui/Android.mk
+++ gui/Android.mk
@@ -113,11 +113,16 @@ endif
 ifeq ($(TW_ROUND_SCREEN), true)
     LOCAL_CFLAGS += -DTW_ROUND_SCREEN
 endif
+<<<<<<< HEAD   (573661 Allow all extension while checking for digest verification)
 ifeq ($(TW_EXCLUDE_NANO), true)
     LOCAL_CFLAGS += -DTW_EXCLUDE_NANO
 endif
 ifeq ($(AB_OTA_UPDATER),true)
     LOCAL_CFLAGS += -DAB_OTA_UPDATER=1
+=======
+ifeq ($(TW_SCREEN_BLANK_ON_BOOT), true)
+    LOCAL_CFLAGS += -DTW_SCREEN_BLANK_ON_BOOT
+>>>>>>> CHANGE (28d8de bootable: Change how blank screen works)
 endif
 
 LOCAL_C_INCLUDES += \
