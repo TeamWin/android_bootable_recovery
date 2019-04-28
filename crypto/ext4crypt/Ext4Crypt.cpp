0c7fa3ca9d519eed74ead6c3eb8cdc7a1f77e108
Noah Jacobson <noahajac@gmail.com>
2019-04-28 00:10:07 -0400

    Add support for multi-user decryption
    * Add CLI support using the command "twrp decrypt <PASSWORD> [USER ID]"
    * Add GUI support using the menu Advanced --> Decrypt User
    
    
    if FBE data, TW_IS_FBE(tw_is_fbe) will be set "1". it is the key to make a distinction between FBE and FDE.
    
    when decrypt FBE data, tw_crypto_user_id should be user id and not null. And decrypt page display the user to decrypt.
    
    when backup data, some dirs of undecrypted users will be exclude. so it will avoid tar down when backup data.
    
    
    
    Change-Id: Id2681bc2b1012a0571417f7a6e0b980b1b25c61c

--- crypto/ext4crypt/Ext4Crypt.cpp
+++ crypto/ext4crypt/Ext4Crypt.cpp
@@ -363,6 +363,9 @@ static bool load_all_de_keys() {
             if (!install_key(key, &raw_ref)) return false;
             s_de_key_raw_refs[user_id] = raw_ref;
             LOG(DEBUG) << "Installed de key for user " << user_id;
+
+            std::string user_prop = "twrp.user." + std::to_string(user_id) + ".decrypt";
+            property_set(user_prop.c_str(), "0");
         }
     }
     // ext4enc:TODO: go through all DE directories, ensure that all user dirs have the
