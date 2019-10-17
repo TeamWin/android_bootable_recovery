17740421b30c1ff73be1291586ad2e746ee4f1f2
Peter Cai <peter@typeblog.net>
2019-10-17 08:49:10 +0800

    partitionmanager: support delayed adopted storage mount
    * On some devices, the external mmc driver will only load the card after
      a certain delay. The original code will break and fail to mount any
      adopted storage in this case.
    
    * Add a special fstab flag to allow the partitionmanager to retry after
      a delay.
    
    Change-Id: I219978850ff3ba7ad9dc7ca96fc30634c9574164

--- partition.cpp
+++ partition.cpp
@@ -160,6 +160,7 @@ enum TW_FSTAB_FLAGS {
 	TWFLAG_RESIZE,
 	TWFLAG_KEYDIRECTORY,
 	TWFLAG_WRAPPEDKEY,
+	TWFLAG_ADOPTED_MOUNT_DELAY,
 };
 
 /* Flags without a trailing '=' are considered dual format flags and can be
@@ -204,6 +205,7 @@ const struct flag_list tw_flags[] = {
 	{ "resize",                 TWFLAG_RESIZE },
 	{ "keydirectory=",          TWFLAG_KEYDIRECTORY },
 	{ "wrappedkey",             TWFLAG_WRAPPEDKEY },
+	{ "adopted_mount_delay=",   TWFLAG_ADOPTED_MOUNT_DELAY },
 	{ 0,                        0 },
 };
 
@@ -267,7 +269,11 @@ TWPartition::TWPartition() {
 	Adopted_GUID = "";
 	SlotSelect = false;
 	Key_Directory = "";
+<<<<<<< HEAD   (a39385 twrpapp: actually include in build)
 	Is_Super = false;
+=======
+	Adopted_Mount_Delay = 0;
+>>>>>>> CHANGE (217208 partitionmanager: support delayed adopted storage mount)
 }
 
 TWPartition::~TWPartition(void) {
@@ -1005,6 +1011,9 @@ void TWPartition::Apply_TW_Flag(const unsigned flag, const char* str, const bool
 			break;
 		case TWFLAG_ALTDEVICE:
 			Alternate_Block_Device = str;
+			break;
+		case TWFLAG_ADOPTED_MOUNT_DELAY:
+			Adopted_Mount_Delay = atoi(str);
 			break;
 		case TWFLAG_KEYDIRECTORY:
 			Key_Directory = str;
