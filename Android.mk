2d8e2c8c73884db50e19bbe1801ad4cc33635774
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2019-12-30 15:19:03 +0100

    fix 'system/etc/ld.config.txt' not found error
    According to https://android.googlesource.com/platform/system/core/+/refs/tags/android-9.0.0_r51/rootdir/Android.mk#260 the ld.config.txt gets generated with the vndk version as a suffix.
    
    The issue seems to be exist since commit 8d70eb617ea2260ba7338169a61f7aa4f5269f61 in twrp.
    
    Change-Id: I19741c29cda65ce051b7c167a946d551ce149f7b

--- Android.mk
+++ Android.mk
@@ -524,7 +524,7 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26; echo $$?),0)
     else
         LOCAL_POST_INSTALL_CMD += \
 	    sed 's/\(namespace.default.search.paths\)\s\{1,\}=/namespace.default.search.paths  = \/sbin\n\1 +=/' \
-                $(TARGET_OUT_ETC)/ld.config.txt > $(TARGET_RECOVERY_ROOT_OUT)/sbin/ld.config.txt;
+                $(TARGET_OUT_ETC)/ld.config.${PLATFORM_VNDK_VERSION}.txt > $(TARGET_RECOVERY_ROOT_OUT)/sbin/ld.config.txt;
     endif
 endif
 
