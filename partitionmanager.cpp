4e7b9b84134d919f7005f05f35e6ab25cbc36bed
Chaosmaster <chaosmaster@chaosv.de>
2020-01-27 00:17:04 +0100

    Implement autodetection for SAR, based on the installed system
    This will detect whether SAR is present in the currently installed
    OS and set the property ro.twrp.sar accordingly.
    After setting the property it will call the bootscript
    /sbin/sarsetup.sh (if present) to give device maintainers the
    option to do setup operations depending on SAR-status, such
    as modifiyng the fstab.
    
    If no system is detected it will fall back to using
    ro.build.system_root_image as basis for deciding
    whether SAR is necessary or not.
    
    Change-Id: I2a48c6c81a6ea6fad6e452c06bfbe4d9da0f1e5c

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -111,7 +111,7 @@ TWPartitionManager::TWPartitionManager(void) {
 #endif
 }
 
-int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error) {
+int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error, bool Sar_Detect) {
 	FILE *fstabFile;
 	char fstab_line[MAX_FSTAB_LINE_LENGTH];
 	TWPartition* settings_partition = NULL;
@@ -245,7 +245,13 @@ int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error)
 			(*iter)->Has_Android_Secure = false;
 
 		if ((*iter)->Mount_Point == "/" || (*iter)->Mount_Point == "/system" || (*iter)->Mount_Point == "/system_root") {
-			(*iter)->Mount_Point = Get_Android_Root_Path();
+			if (Sar_Detect) {
+				(*iter)->Mount_Point = "/s";
+				(*iter)->Mount_Read_Only = true;
+				return true;
+			}
+			else
+				(*iter)->Mount_Point = Get_Android_Root_Path();
 		}
 	}
 
@@ -2835,7 +2841,7 @@ string TWPartitionManager::Get_Active_Slot_Display() {
 }
 
 string TWPartitionManager::Get_Android_Root_Path() {
-	if (property_get_bool("ro.build.system_root_image", false))
+	if (property_get_bool("ro.twrp.sar", false))
 		return "/system_root";
 	return "/system";
 }
