f27b95f5b4f6d4f67edb37371d56f1a07284e578
Mohd Faraz <androiabledroid@gmail.com>
2020-08-12 12:29:42 +0000

    Added Support for terminal from filemanager
     * Now We can open terminal from current directory in filemanager
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>
    Change-Id: I6df9ee1222c1fa24105da1002abe3c180be2ded1
    Signed-off-by: AndroiableDroid <mohd.faraz.abc@gmail.com>

--- gui/action.cpp
+++ gui/action.cpp
@@ -67,6 +67,7 @@ static string zip_queue[10];
 static int zip_queue_index;
 pid_t sideload_child_pid;
 extern std::vector<users_struct> Users_List;
+extern GUITerminal* term;
 
 static void *ActionThread_work_wrapper(void *data);
 
@@ -205,6 +206,7 @@ GUIAction::GUIAction(xml_node<>* node)
 		ADD_ACTION(togglebacklight);
 		ADD_ACTION(enableadb);
 		ADD_ACTION(enablefastboot);
+		ADD_ACTION(change_terminal);
 
 		// remember actions that run in the caller thread
 		for (mapFunc::const_iterator it = mf.begin(); it != mf.end(); ++it)
@@ -2285,5 +2287,26 @@ int GUIAction::enableadb(std::string arg __unused) {
 int GUIAction::enablefastboot(std::string arg __unused) {
 	android::base::SetProperty("sys.usb.config", "none");
 	android::base::SetProperty("sys.usb.config", "fastboot");
+	return 0;
+}
+
+int GUIAction::change_terminal(std::string arg) {
+	bool res = true;
+	char* buf = nullptr;
+	if (term != NULL && !arg.empty()) {
+		if (term->status()) {
+			term->stop();
+		}
+		if (chdir(arg.c_str()) != 0) {
+			LOGINFO("Unable to change dir to %s\n", arg.c_str());
+			res = false;
+		}
+	}
+	else {
+		res = false;
+		LOGINFO("error\n");
+	}
+	if (res)
+		gui_changePage("terminalcommand");
 	return 0;
 }
