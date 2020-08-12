8c8ae72ef2608993dd7704eaece6a21616bbfa8a
Mohd Faraz <androiabledroid@gmail.com>
2020-08-12 12:29:42 +0000

    Added Support for terminal from filemanager
     * Now We can open terminal from current directory in filemanager
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>
    Change-Id: I6df9ee1222c1fa24105da1002abe3c180be2ded1

--- gui/objects.hpp
+++ gui/objects.hpp
@@ -371,6 +371,7 @@ protected:
 	int uninstalltwrpsystemapp(std::string arg);
 	int repackimage(std::string arg);
 	int fixabrecoverybootloop(std::string arg);
+	int change_terminal(std::string arg);
 
 	int simulate;
 };
