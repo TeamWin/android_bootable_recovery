97376bbc302cf82dfc06d64f1884508e1011f033
Mohd Faraz <androiabledroid@gmail.com>
2020-08-14 01:40:59 +0000

    Fix up issue during decryption
     * Since some devices are unable to decrypt after processing of the fstab,
       because of the partition details updation, and after intro of dynamic partitioning
       here decryption process was going on after complete processing of the dynamic volumes.
    
     * This patch fix these issue, it process the logical volumes at their time not after all processing fstabs.
       This fixes the decryption issue.
       Translate more partitions i.e., ODM, Product.
       Added more partitions in unmount partitions.
    
    Change-Id: I977c0cf0c40e5311f54a78c98f1fd89f71c6ac57
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- twrp.cpp
+++ twrp.cpp
@@ -77,6 +77,8 @@ static void Decrypt_Page(bool SkipDecryption, bool datamedia) {
 			LOGINFO("Skipping decryption\n");
 		} else {
 			LOGINFO("Is encrypted, do decrypt page first\n");
+			if (DataManager::GetIntValue(TW_IS_FBE))
+				DataManager::SetValue("tw_crypto_user_id", "0");
 			if (gui_startPage("decrypt", 1, 1) != 0) {
 				LOGERR("Failed to start decrypt GUI page.\n");
 			} else {
@@ -224,19 +226,6 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 	PartitionManager.Output_Partition_Logging();
-#ifdef TW_INCLUDE_CRYPTO
-	DataManager::SetValue(TW_IS_ENCRYPTED, 1);
-#endif
-
-	if (PartitionManager.Get_Super_Status()) {
-		PartitionManager.Setup_Super_Devices();
-		PartitionManager.Setup_Super_Partition();
-	} else {
-#ifdef TW_INCLUDE_CRYPTO
-		if (!PartitionManager.Get_Super_Status())
-			PartitionManager.Decrypt_Data();
-#endif
-	}
 
 	// Load up all the resources
 	gui_loadResources();
@@ -345,8 +334,7 @@ int main(int argc, char **argv) {
 	LOGINFO("Backup of TWRP ramdisk done.\n");
 #endif
 
-	if (!PartitionManager.Get_Super_Status())
-		Decrypt_Page(SkipDecryption, datamedia);
+	Decrypt_Page(SkipDecryption, datamedia);
 
 	// Fixup the RTC clock on devices which require it
 	if (crash_counter == 0)
@@ -398,9 +386,6 @@ int main(int argc, char **argv) {
 	TWPartition* ven = PartitionManager.Find_Partition_By_Path("/vendor");
 	if (sys) {
 		if (sys->Get_Super_Status()) {
-			if (!PartitionManager.Prepare_All_Super_Volumes()) {
-				LOGERR("Unable to prepare super volumes.\n");
-			}
 			sys->Mount(true);
 			if (ven) {
 				ven->Mount(true);
@@ -411,8 +396,6 @@ int main(int argc, char **argv) {
 			}
 			property_set("twrp.apex.loaded", "true");
 #ifdef TW_INCLUDE_CRYPTO
-			PartitionManager.Decrypt_Data();
-			Decrypt_Page(SkipDecryption, datamedia);
 			std::string recoveryLogDir(DATA_LOGS_DIR);
 			recoveryLogDir += "/recovery";
 			if (!TWFunc::Path_Exists(recoveryLogDir)) {
