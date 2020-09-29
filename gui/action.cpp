73f9825e28d6edf1e04d88f79fb7d5f67c7faf88
Mohd Faraz <androiabledroid@gmail.com>
2020-09-30 00:21:57 +0530

    Unmount apex before flashing
    BUG: there was mounting issues with migrate-ng with mounted apex.
    
    TEST: On joyeuse migrate-ng flashing without any mounting issue.
    
    Change-Id: I689128d85aa456dc4b2bc6b010efb5bfcda7c546
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- gui/action.cpp
+++ gui/action.cpp
@@ -40,7 +40,6 @@
 #include "../twrp-functions.hpp"
 #include "../twrpRepacker.hpp"
 #include "../openrecoveryscript.hpp"
-
 #include "install/adb_install.h"
 
 #include "fuse_sideload.h"
@@ -1070,6 +1069,11 @@ int GUIAction::flash(std::string arg)
 		DataManager::SetValue(TW_ZIP_INDEX, (i + 1));
 
 		TWFunc::SetPerformanceMode(true);
+
+		if (PartitionManager.Get_Super_Status()) {
+			PartitionManager.apex.unloadApexImages();
+			PartitionManager.UnMount_Main_Partitions(); //unmount main partitions before flashing a zip, cuz we dunno the behaviour of the zip
+		}
 		ret_val = flash_zip(zip_path, &wipe_cache);
 		TWFunc::SetPerformanceMode(false);
 		if (ret_val != 0) {
@@ -1087,6 +1091,8 @@ int GUIAction::flash(std::string arg)
 
 	reinject_after_flash();
 	PartitionManager.Update_System_Details();
+	if (PartitionManager.Get_Super_Status()) // Remount apex
+		PartitionManager.apex.reloadApexImages();
 	operation_end(ret_val);
 	// This needs to be after the operation_end call so we change pages before we change variables that we display on the screen
 	DataManager::SetValue(TW_ZIP_QUEUE_COUNT, zip_queue_index);
