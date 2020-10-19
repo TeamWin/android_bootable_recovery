e92511b1bf03a2bb5a8e9a71053d5e52df626901
Captain Throwback <captainthrowback@hotmail.com>
2020-10-19 11:13:53 -0400

    init: includes more services in TWRP by default
    vndservicemanager should be included with crypto support
    
    boot and health services are needed for bootctrl and fastbootd
    
    hwservicemanager and servicemanager should be included
    regardless of crypto status
    
    Change-Id: Ie76dac11b733c4549c2d55301cf614fe82905884

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -79,6 +79,7 @@ RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../system/bin/keystore_cli_v2
 RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../system/bin/hwservicemanager
 RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../system/bin/servicemanager
 RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../system/bin/vold_prepare_subdirs
+RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../vendor/bin/hw/android.hardware.boot@1.0-service
 RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../vendor/bin/vndservicemanager
 RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../vendor/bin/hw/android.hardware.health@2.0-service
 RECOVERY_BINARY_SOURCE_FILES += $(TARGET_ROOT_OUT)/../system/bin/toybox
