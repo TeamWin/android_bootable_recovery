93eb2ee4e248edc926a49a83fdc9b6372f972ee2
nebrassy <nebras30@gmail.com>
2020-10-24 21:08:20 +0200

    set crypto state/type when detected
    Change-Id: I4506d3ef11052dbebf974b5c387a3c7d5055f2ea

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -390,6 +390,7 @@ void TWPartitionManager::Decrypt_Data() {
 	#ifdef TW_INCLUDE_CRYPTO
 	TWPartition* Decrypt_Data = Find_Partition_By_Path("/data");
 	if (Decrypt_Data && Decrypt_Data->Is_Encrypted && !Decrypt_Data->Is_Decrypted) {
+		property_set("ro.crypto.state", "encrypted");
 		if (!Decrypt_Data->Key_Directory.empty() && Mount_By_Path(Decrypt_Data->Key_Directory, false)) {
 #ifdef TW_INCLUDE_FBE_METADATA_DECRYPT
 #ifdef USE_FSCRYPT
@@ -402,7 +403,6 @@ void TWPartitionManager::Decrypt_Data() {
 				LOGINFO("Successfully decrypted metadata encrypted data partition with new block device: '%s'\n", 
 				Decrypt_Data->Decrypted_Block_Device.c_str());
 #endif
-				property_set("ro.crypto.state", "encrypted");
 				Decrypt_Data->Is_Decrypted = true; // Needed to make the mount function work correctly
 				int retry_count = 10;
 				while (!Decrypt_Data->Mount(false) && --retry_count)
