ba2b1043300a78be6041344062cee56956dc7a17
epicX <souravgain605@gmail.com>
2021-01-04 13:01:31 +0530

    Add support to change TWRP Folder
    Change-Id: Id3b3b46d492bf83d1d9d3f535c880ea9d15b4107

--- variables.h
+++ variables.h
@@ -19,6 +19,8 @@
 
 #define TW_MAIN_VERSION_STR       "3.5.0_9"
 #define TW_VERSION_STR TW_MAIN_VERSION_STR TW_DEVICE_VERSION
+#define TW_SETTINGS_FILE            ".twrps"
+#define TW_DEFAULT_FOLDER           "/TWRP"
 
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"
@@ -78,6 +80,7 @@
 #define TW_RM_RF_VAR                "tw_rm_rf"
 
 #define TW_BACKUPS_FOLDER_VAR       "tw_backups_folder"
+#define TW_MAIN_FOLDER_VAR          "tw_main_folder"
 
 #define TW_SDEXT_SIZE               "tw_sdext_size"
 #define TW_SWAP_SIZE                "tw_swap_size"
