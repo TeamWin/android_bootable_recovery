049ca21bf9ba7fbe1451531095a1846000860d3a
epicX <souravgain605@gmail.com>
2021-01-04 13:01:31 +0530

    Add support to change TWRP Folder
    Change-Id: Id3b3b46d492bf83d1d9d3f535c880ea9d15b4107

--- gui/action.cpp
+++ gui/action.cpp
@@ -235,6 +235,7 @@ GUIAction::GUIAction(xml_node<>* node)
 		ADD_ACTION(uninstalltwrpsystemapp);
 		ADD_ACTION(repackimage);
 		ADD_ACTION(fixabrecoverybootloop);
+		ADD_ACTION(applycustomtwrpfolder);
 	}
 
 	// First, get the action
@@ -2216,5 +2217,55 @@ int GUIAction::fixabrecoverybootloop(std::string arg __unused)
 	op_status = 0;
 exit:
 	operation_end(op_status);
+	return 0;
+}
+
+int GUIAction::applycustomtwrpfolder(string arg __unused){
+	operation_start("ChangingTWRPFolder");
+	string storageFolder = DataManager::GetSettingsStoragePath();
+	string settingsFile = DataManager::GenarateSettingsFileName();
+
+	string newFolder = storageFolder + '/' + arg;
+	string newBackupFolder = newFolder + "/BACKUPS/" + DataManager::GetStrValue("device_id");
+	string prevFolder = storageFolder + DataManager::GetStrValue(TW_MAIN_FOLDER_VAR);
+
+	bool ret = false;
+
+	if(TWFunc::Path_Exists(newFolder)){
+		gui_msg(Msg(msg::kError, "tw_folder_exists=A folder with that name already exists!"));
+	}else{
+		ret = true;
+	}
+
+	if(newFolder != prevFolder && ret){
+		
+		ret = TWFunc::Exec_Cmd("mv -f \"" + prevFolder + "\" \"" + newFolder + '\"') != 0 ? false : true;
+		
+		string newSettingFile = newFolder + '/' + settingsFile;
+		string oldSettingFile = newFolder + "/.twrps";
+		
+		if(arg == "TWRP" && ret){
+			if(TWFunc::Path_Exists(newSettingFile)){
+				TWFunc::Exec_Cmd("mv -f \"" + newSettingFile + "\" \"" + oldSettingFile + '\"');
+			}
+		}else if(ret){
+			if(TWFunc::Path_Exists(oldSettingFile)){
+				TWFunc::Exec_Cmd("mv -f \"" + oldSettingFile + "\" \"" + newSettingFile + '\"');
+			}
+		}
+
+	}else{
+		gui_msg(Msg(msg::kError, "tw_folder_exists=A folder with that name already exists!"));
+	}
+
+	if(ret) ret = TWFunc::Exec_Cmd("mkdir -p \"" + newBackupFolder + '\"') != 0 ? false : true;
+
+
+	if(ret){
+		DataManager::SetValue(TW_MAIN_FOLDER_VAR, '/' + arg);
+		DataManager::SetValue(TW_BACKUPS_FOLDER_VAR, newBackupFolder);
+		DataManager::mBackingFile = arg == "TWRP" ? newFolder + "/.twrps" : newFolder + '/' + settingsFile;
+	}
+	operation_end((int)!ret);
 	return 0;
 }
