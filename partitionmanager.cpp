d01021070ea438d13bf3895c65243d438e53df07
epicX <souravgain605@gmail.com>
2021-01-04 13:01:31 +0530

    Add support to change TWRP Folder
    Change-Id: Id3b3b46d492bf83d1d9d3f535c880ea9d15b4107

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -1696,6 +1696,7 @@ void TWPartitionManager::Post_Decrypt(const string& Block_Device) {
 			DataManager::SetValue("tw_settings_path", "/data/media/0");
 			dat->UnMount(false);
 		}
+		DataManager::LoadTWRP_FolderInfo();
 		Update_System_Details();
 		Output_Partition(dat);
 		UnMount_Main_Partitions();
