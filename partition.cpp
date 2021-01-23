96baf1f8f703b0d745ebedd39d50d20002c4ba03
bigbiff <bigbiff@teamw.in>
2021-01-23 14:08:03 -0500

    android root: restore context for android system root
    In this patchset we will store and reset the android system
    root context after a wipe and restore to prevent issues
    with no context labels in the file_contexts being applied.
    This will prevent no boot issues in android with selinux
    denials on the system root partition.
    
    Change-Id: I87fd3a6060fbaa3e7bdfd7a4b1d09feeb4fa3f27
    (cherry picked from commit 35d2bfda3c89ebd212a9bd2328bffd7464f95d47)

--- partition.cpp
+++ partition.cpp
@@ -1672,6 +1672,20 @@ bool TWPartition::Wipe(string New_File_System) {
 	if (Mount_Point == "/cache")
 		Log_Offset = 0;
 
+<<<<<<< HEAD   (045f34 cryptfs: add support for keymaster 2)
+=======
+	if (Mount_Point == PartitionManager.Get_Android_Root_Path()) {
+		if (tw_get_default_metadata(PartitionManager.Get_Android_Root_Path().c_str()) != 0) {
+			gui_msg(Msg(msg::kWarning, "restore_system_context=Unable to get default context for {1} -- Android may not boot.")(PartitionManager.Get_Android_Root_Path()));
+		}
+	}
+
+	if (Retain_Layout_Version && Mount(false) && TWFunc::Path_Exists(Layout_Filename))
+		TWFunc::copy_file(Layout_Filename, "/.layout_version", 0600);
+	else
+		unlink("/.layout_version");
+
+>>>>>>> CHANGE (35d2bf android root: restore context for android system root)
 	if (Has_Data_Media && Current_File_System == New_File_System) {
 		wiped = Wipe_Data_Without_Wiping_Media();
 		if (Mount_Point == "/data" && TWFunc::get_log_dir() == DATA_LOGS_DIR) {
@@ -1711,6 +1725,15 @@ bool TWPartition::Wipe(string New_File_System) {
 		if (Mount_Point == "/cache" && TWFunc::get_log_dir() != DATA_LOGS_DIR)
 			DataManager::Output_Version();
 
+<<<<<<< HEAD   (045f34 cryptfs: add support for keymaster 2)
+=======
+		if (Mount_Point == PartitionManager.Get_Android_Root_Path()) {
+			tw_set_default_metadata(PartitionManager.Get_Android_Root_Path().c_str());
+		}
+		if (TWFunc::Path_Exists("/.layout_version") && Mount(false))
+			TWFunc::copy_file("/.layout_version", Layout_Filename, 0600);
+
+>>>>>>> CHANGE (35d2bf android root: restore context for android system root)
 		if (update_crypt) {
 			Setup_File_System(false);
 			if (Is_Encrypted && !Is_Decrypted) {
