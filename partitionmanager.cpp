ad55cb8d284287f079fcbc0d2f167857f30f8321
bigbiff <bigbiff@teamw.in>
2021-03-07 18:20:29 -0500

    FsCrypt update WIP
    Change-Id: I62a29c1bef36c259ec4b11259f71be613d20a112

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -92,12 +92,10 @@ extern "C" {
 #include "gui/rapidxml.hpp"
 #include "gui/pages.hpp"
 #ifdef TW_INCLUDE_FBE
-#include "crypto/ext4crypt/Decrypt.h"
+#include "Decrypt.h"
 #ifdef TW_INCLUDE_FBE_METADATA_DECRYPT
 	#ifdef USE_FSCRYPT
-	#include "crypto/fscrypt/MetadataCrypt.h"
-	#else
-	#include "crypto/ext4crypt/MetadataCrypt.h"
+	#include "MetadataCrypt.h"
 	#endif
 #endif
 #endif
@@ -404,13 +402,9 @@ void TWPartitionManager::Decrypt_Data() {
 #ifdef TW_INCLUDE_FBE_METADATA_DECRYPT
 #ifdef USE_FSCRYPT
 			if (fscrypt_mount_metadata_encrypted(Decrypt_Data->Actual_Block_Device, Decrypt_Data->Mount_Point, false)) {
-				std::string crypto_blkdev =android::base::GetProperty("ro.crypto.fs_crypto_blkdev", "error");
+				std::string crypto_blkdev = android::base::GetProperty("ro.crypto.fs_crypto_blkdev", "error");
 				Decrypt_Data->Decrypted_Block_Device = crypto_blkdev;
 				LOGINFO("Successfully decrypted metadata encrypted data partition with new block device: '%s'\n", crypto_blkdev.c_str());
-#else
-			if (e4crypt_mount_metadata_encrypted(Decrypt_Data->Mount_Point, false, Decrypt_Data->Key_Directory, Decrypt_Data->Actual_Block_Device, &Decrypt_Data->Decrypted_Block_Device)) {
-				LOGINFO("Successfully decrypted metadata encrypted data partition with new block device: '%s'\n", 
-				Decrypt_Data->Decrypted_Block_Device.c_str());
 #endif
 				Decrypt_Data->Is_Decrypted = true; // Needed to make the mount function work correctly
 				int retry_count = 10;
