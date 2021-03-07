846cb256054532fef557d9be7ea2a3a42019731e
bigbiff <bigbiff@teamw.in>
2021-03-07 18:20:29 -0500

    FsCrypt update WIP
    Change-Id: I62a29c1bef36c259ec4b11259f71be613d20a112

--- libtar/Android.mk
+++ libtar/Android.mk
@@ -14,14 +14,12 @@ LOCAL_C_INCLUDES += external/libselinux/include
 LOCAL_SHARED_LIBRARIES += libselinux
 
 ifeq ($(TW_INCLUDE_CRYPTO_FBE), true)
-    ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 29; echo $$?),0)
-        LOCAL_SHARED_LIBRARIES += libtwrpfscrypt
-        LOCAL_CFLAGS += -DUSE_FSCRYPT
-        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/fscrypt
+    LOCAL_SHARED_LIBRARIES += libtwrpfscrypt
+    LOCAL_CFLAGS += -DUSE_FSCRYPT
+    ifeq ($(TW_USE_KM4), true)
+        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/fscrypt-km4
     else
-        LOCAL_SHARED_LIBRARIES += libe4crypt
-        LOCAL_CFLAGS += -DHAVE_EXT4_CRYPT
-        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/ext4crypt
+        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/fscrypt-km4.1
     endif
 endif
 
@@ -45,14 +43,12 @@ LOCAL_C_INCLUDES += external/libselinux/include
 LOCAL_STATIC_LIBRARIES += libselinux
 
 ifeq ($(TW_INCLUDE_CRYPTO_FBE), true)
-    ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 29; echo $$?),0)
-        LOCAL_SHARED_LIBRARIES += libtwrpfscrypt
-        LOCAL_CFLAGS += -DUSE_FSCRYPT
-        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/fscrypt
+    LOCAL_SHARED_LIBRARIES += libtwrpfscrypt
+    LOCAL_CFLAGS += -DUSE_FSCRYPT
+    ifeq ($(TW_USE_KM4), true)
+        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/fscrypt-km4
     else
-        LOCAL_SHARED_LIBRARIES += libe4crypt
-        LOCAL_CFLAGS += -DHAVE_EXT4_CRYPT
-        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/ext4crypt
+        LOCAL_C_INCLUDES += $(LOCAL_PATH)/../crypto/fscrypt-km4.1
     endif
 endif
 
