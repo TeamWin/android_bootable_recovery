c6e82ef90e75ae676694312f1193b830babc257a
bigbiff <bigbiff@teamw.in>
2021-06-20 16:20:27 -0400

    vab: check for pending merges before format data
    Change-Id: I207f1d3db9f3184654aefb7bd0a0ee23c9e58a64

--- Android.mk
+++ Android.mk
@@ -148,7 +148,10 @@ endif
 
 ifeq ($(AB_OTA_UPDATER),true)
     LOCAL_CFLAGS += -DAB_OTA_UPDATER=1
-    LOCAL_SHARED_LIBRARIES += libhardware android.hardware.boot@1.0 android.hardware.boot@1.1
+    LOCAL_C_INCLUDES += system/core/fs_mgr/libsnapshot/include
+    LOCAL_SHARED_LIBRARIES += libhardware android.hardware.boot@1.0 android.hardware.boot@1.1 libprotobuf-cpp-lite
+    LOCAL_STATIC_LIBRARIES += libsnapshot_nobinder libhidlbase libutils liblog libfs_mgr liblp libfec libsquashfs_utils \
+        libcrypto_utils libfec_rs
     TWRP_REQUIRED_MODULES += libhardware android.hardware.boot@1.0-service android.hardware.boot@1.0-service.rc \
     android.hardware.boot@1.1-service android.hardware.boot@1.1-service.rc android.hardware.boot@1.1.xml
 endif
--- partitions.hpp
+++ partitions.hpp
@@ -227,6 +227,7 @@ private:
 	bool Flash_Sparse_Image(const string& Filename);                          // Flashes a sparse image using simg2img
 	bool Flash_Image_FI(const string& Filename, ProgressTracking *progress);  // Flashes an image to the partition using flash_image for mtd nand
 	void ExcludeAll(const string& path);                                      // Adds an exclusion for path to both the backup and wipe exclusion lists
+	bool Check_Pending_Merges();
 
 private:
 	bool Can_Be_Mounted;                                                      // Indicates that the partition can be mounted
