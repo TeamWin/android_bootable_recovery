6ebf60a5967f9df28a75cf23dfdb35f1ec50d7e5
spyderbig <spyderbig@gerrit.twrp.me>
2021-08-22 21:06:16 +0000

    git fetch https://spyderbig@gerrit.twrp.me/a/android_bootable_recovery refs/changes/92/3892/22 && git checkout -b change-3892 FETCH_HEAD
    Change-Id: I31b20b668f7e49b4ae312acd34bbe7b5cdb578e6

--- twrpinstall/twinstall.cpp
+++ twrpinstall/twinstall.cpp
@@ -55,7 +55,6 @@
 #include "gui/pages.hpp"
 #include "twinstall.h"
 #include "installcommand.h"
-#include "../twrpRepacker.hpp"
 extern "C" {
 	#include "gui/gui.h"
 }
@@ -243,7 +242,7 @@ static int Run_Update_Binary(const char *path, int* wipe_cache, zip_type ztype) 
 }
 
 int TWinstall_zip(const char* path, int* wipe_cache, bool check_for_digest) {
-	int ret_val, zip_verify = 1, unmount_system = 1, reflashtwrp = 0;
+	int ret_val, zip_verify = 1, unmount_system = 1;
 
 	gui_msg(Msg("installing_zip=Installing zip file '{1}'")(path));
 	if (strlen(path) < 9 || strncmp(path, "/sideload", 9) != 0) {
@@ -351,11 +350,6 @@ int TWinstall_zip(const char* path, int* wipe_cache, bool check_for_digest) {
 				gui_warn("mount_vab_partitions=Devices on super may not mount until rebooting recovery.");
 			}
 			gui_warn("flash_ab_reboot=To flash additional zips, please reboot recovery to switch to the updated slot.");
-			DataManager::GetValue(TW_AUTO_REFLASHTWRP_VAR, reflashtwrp);
-			if (reflashtwrp) {
-			twrpRepacker repacker;
-			repacker.Flash_Current_Twrp();
-			}
 		} else {
 			std::string binary_name("ui.xml");
 			ZipEntry binary_entry;
