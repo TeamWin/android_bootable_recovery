147d27f5db4738bcc2a7aa350af23028bc7de1c9
Captain Throwback <captainthrowback@hotmail.com>
2021-09-04 10:59:26 -0400

    etc: include hwservicemanager for TW_INCLUDE_CRYPTO
    A recent change removed the inclusion of hwservicemanager
    and vndservicemanager with only this flag
    
    Change-Id: I9b8cb05a6bd053b794599167bcc8bf0591b2e184

--- etc/Android.mk
+++ etc/Android.mk
@@ -67,7 +67,12 @@ LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/etc/init
 LOCAL_SRC_FILES := init/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+<<<<<<< HEAD   (afb12e repacker: adjust unpack command to fix compression issues)
 ifeq ($(filter $(AB_OTA_UPDATER) $(PRODUCT_USE_DYNAMIC_PARTITIONS), true), true)
+=======
+
+ifneq ($(filter $(AB_OTA_UPDATER) $(PRODUCT_USE_DYNAMIC_PARTITIONS) $(TW_INCLUDE_CRYPTO), true),)
+>>>>>>> CHANGE (bb4ee7 etc: include hwservicemanager for TW_INCLUDE_CRYPTO)
 	include $(CLEAR_VARS)
 	LOCAL_MODULE := hwservicemanager.rc
 	LOCAL_MODULE_TAGS := optional
