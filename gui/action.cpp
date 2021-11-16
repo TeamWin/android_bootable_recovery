4392b80a0678b1076367edf885dbbfb0be408639
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-11-16 08:56:12 +0200

    action: Only unmount /vendor if it actually exists
    * Since commit 168bc4caaedfdf655a71ba24a1c5a7f2b905a122 the vendor partition gets unmounted prior to switching the slots. However, devices without a vendor partition (e.g. tissot's stock partition table) will have errors since the partition doesn't actually exist
    * Therefore, it should be only mounted if that partition exists in the partition table
    
    Change-Id: Id16ab55e25d05964ca0de1bf218914d66c2b0205

--- gui/action.cpp
+++ gui/action.cpp
@@ -1983,14 +1983,14 @@ int GUIAction::setbootslot(std::string arg)
 {
 	operation_start("Set Boot Slot");
 	if (!simulate) {
-		PartitionManager.UnMount_By_Path("/vendor",true);
+		if (PartitionManager.Find_Partition_By_Path("/vendor"))
+			PartitionManager.UnMount_By_Path("/vendor",true);
 		PartitionManager.Set_Active_Slot(arg);
 	} else
 		simulate_progress_bar();
 	operation_end(0);
 	return 0;
 }
-
 int GUIAction::checkforapp(std::string arg __unused)
 {
 	operation_start("Check for TWRP App");
