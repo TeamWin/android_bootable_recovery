4fb3d206d17b4f69606460cc55f75909331c50f7
Yillié <yilliee@protonmail.com>
2022-06-21 08:47:34 +0500

    Allow bottom-aligning status bar icons
    - If the var TW_STATUS_ICONS_BOTTOM_ALIGN is set to true it sets
      the value of row1_header_y such that the bottom of the icons
      corresponds to the bottom of the status bar.
      ( Value calculated by : statusbar height - size of font_m ).
    - If it's not set to true, the value of row1_header_y defaults
      to it's old value.
    
    Change-Id: I1b292221f3c34bbac20233595fbc0958d4aa4cc1
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- gui/libguitwrp_defaults.go
+++ gui/libguitwrp_defaults.go
@@ -97,6 +97,11 @@ func copyThemeResources(ctx android.BaseContext, dirs []string, files []string) 
 			newFile = strings.Replace(newFile, "{battery_pos}", props[0], -1)
 			newFile = strings.Replace(newFile, "{clock_pos}", props[1], -1)
 			newFile = strings.Replace(newFile, "{cpu_pos}", props[2], -1)
+			alignProp := "%status_topalign_header_y%"
+			if getMakeVars(ctx, "TW_STATUS_ICONS_BOTTOM_ALIGN") == "true" {
+				alignProp = "%status_bottomalign_header_y%"
+			}
+			newFile = strings.Replace(newFile, "{statusicons_align}", alignProp, -1)
 		}
 		err = ioutil.WriteFile(twRes + i, []byte(newFile), 0)
 		if err != nil {
