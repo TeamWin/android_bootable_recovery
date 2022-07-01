7d025d22009a50d5791aa003de1ca9eb013fa70c
Diniboy1123 <diniboy@protonmail.com>
2022-07-01 20:11:13 +0300

    recovery: Added F2FS compression support for wiping
    Change-Id: I73253bb63aa3cae9852a96d170212608b2e0ba2a
    Signed-off-by: Diniboy1123 <diniboy@protonmail.com>

--- partition.cpp
+++ partition.cpp
@@ -136,6 +136,7 @@ enum TW_FSTAB_FLAGS {
 	TWFLAG_METADATA_ENCRYPTION,
 	TWFLAG_FLASHIMG,
 	TWFLAG_FORCEENCRYPT,
+	TWFLAG_FSCOMPRESS,
 	TWFLAG_FSFLAGS,
 	TWFLAG_IGNOREBLKID,
 	TWFLAG_LENGTH,
@@ -185,6 +186,7 @@ const struct flag_list tw_flags[] = {
 	{ "metadata_encryption=",   TWFLAG_METADATA_ENCRYPTION },
 	{ "flashimg",               TWFLAG_FLASHIMG },
 	{ "forceencrypt=",          TWFLAG_FORCEENCRYPT },
+	{ "fscompress",             TWFLAG_FSCOMPRESS },
 	{ "fsflags=",               TWFLAG_FSFLAGS },
 	{ "ignoreblkid",            TWFLAG_IGNOREBLKID },
 	{ "length=",                TWFLAG_LENGTH },
@@ -245,6 +247,7 @@ TWPartition::TWPartition() {
 	Backup_Size = 0;
 	Can_Be_Encrypted = false;
 	Is_Encrypted = false;
+	Is_Compressed = false;
 	Is_Decrypted = false;
 	Is_FBE = false;
 	Mount_To_Decrypt = false;
@@ -965,6 +968,9 @@ void TWPartition::Apply_TW_Flag(const unsigned flag, const char* str, const bool
 			break;
 		case TWFLAG_FLASHIMG:
 			Can_Flash_Img = val;
+			break;
+		case TWFLAG_FSCOMPRESS:
+			Is_Compressed = val;
 			break;
 		case TWFLAG_FSFLAGS:
 			Process_FS_Flags(str);
@@ -2457,6 +2463,9 @@ bool TWPartition::Wipe_F2FS() {
 
 	if(needs_casefold)
 		f2fs_command += " -O casefold -C utf8";
+
+	if(Is_Compressed)
+		f2fs_command += " -O compression,extra_attr";
 
 	f2fs_command += " " + Actual_Block_Device + " " + dev_sz_str;
 
