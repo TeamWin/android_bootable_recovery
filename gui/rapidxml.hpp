90ab1735e04d708d887496b47b2bf64656dc74cf
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-21 14:25:53 +0800

    gui: rapidxml: Bail out if detecting ABX header
    The parser is supposed to parse plain XMLs, ABX format is not yet supported.
    Continuing to parse ABX XMLs would be spamming /tmp/recovery.log until triggering OOM.
    
    Change-Id: I5c01045d6efef0b2ac497b40d78410c2b75d30d0

--- gui/rapidxml.hpp
+++ gui/rapidxml.hpp
@@ -1391,6 +1391,14 @@ namespace rapidxml
             // Parse BOM, if any
             parse_bom<Flags>(text);
             
+            // Abort if it's ABX format
+            if (text[0] == Ch('A') &&
+                text[1] == Ch('B') &&
+                text[2] == Ch('X')) {
+                RAPIDXML_PARSE_ERROR("Unsupported ABX Format", text);
+                return;
+            }
+
             // Parse children
             while (1)
             {
