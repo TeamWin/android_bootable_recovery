22e094521f0f4c489e77a1190e28bca8f9767260
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-28 01:21:34 +0800

    partition: Don't force system partitions to read-only
    Let's follow fstab flags.
    
    Change-Id: I9c8d8c9a998df242256e820065716558dc56b908

--- partition.cpp
+++ partition.cpp
@@ -470,7 +470,6 @@ bool TWPartition::Process_Fstab_Line(const char *fstab_line, bool Display_Error,
 			Wipe_Available_in_GUI = false;
 			Can_Be_Backed_Up = false;
 			Can_Be_Wiped = false;
-			Mount_Read_Only = true;
 			Make_Dir(PartitionManager.Get_Android_Root_Path(), true);
 		} else if (Mount_Point == "/product") {
 			Display_Name = "Product";
@@ -478,14 +477,12 @@ bool TWPartition::Process_Fstab_Line(const char *fstab_line, bool Display_Error,
 			Backup_Display_Name = Display_Name;
 			Storage_Name = Display_Name;
 			Can_Be_Backed_Up = Wipe_Available_in_GUI = Is_Super ? false : true;
-			Mount_Read_Only = true;
 		} else if (Mount_Point == "/odm") {
 			Display_Name = "ODM";
 			Backup_Name = "ODM";
 			Backup_Display_Name = Display_Name;
 			Storage_Name = Display_Name;
 			Can_Be_Backed_Up = Wipe_Available_in_GUI = Is_Super ? false : true;
-			Mount_Read_Only = true;
 		} else if (Mount_Point == "/data") {
 			Display_Name = "Data";
 			Backup_Display_Name = Display_Name;
@@ -532,7 +529,6 @@ bool TWPartition::Process_Fstab_Line(const char *fstab_line, bool Display_Error,
 			Display_Name = "Vendor";
 			Backup_Display_Name = Display_Name;
 			Storage_Name = Display_Name;
-			Mount_Read_Only = true;
 		}
 #ifdef TW_EXTERNAL_STORAGE_PATH
 		if (Mount_Point == EXPAND(TW_EXTERNAL_STORAGE_PATH)) {
