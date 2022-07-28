f3d27006985b38cc8b8140a56e8cb4e630cbe2a2
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-29 06:12:38 +0800

    partition: Perform BLKDISCARD on the block device before flashing image
    Change-Id: Ifb13d47693edfc96369053199ac0b5d81910f104

--- partition.cpp
+++ partition.cpp
@@ -1711,6 +1711,18 @@ bool TWPartition::ReMount_RW(bool Display_Error) {
 	return ret;
 }
 
+bool TWPartition::BlkDiscard() {
+#ifdef TW_BLKDISCARD
+	string cmd;
+	LOGINFO("Perform BLKDISCARD on block device %s\n", Actual_Block_Device.c_str());
+	cmd = "/system/bin/toybox blkdiscard " + Actual_Block_Device;
+	return (TWFunc::Exec_Cmd(cmd) == 0);
+#else
+	LOGINFO("BLKDISCARD is disabled.\n");
+	return true;
+#endif
+}
+
 bool TWPartition::Wipe(string New_File_System) {
 	bool wiped = false, update_crypt = false, recreate_media = true;
 	int check;
@@ -2715,6 +2727,8 @@ bool TWPartition::Raw_Read_Write(PartitionSettings *part_settings) {
 	unsigned long long backedup_size = 0;
 	string srcfn, destfn;
 
+	BlkDiscard();
+
 	if (part_settings->PM_Method == PM_BACKUP) {
 		srcfn = Actual_Block_Device;
 		if (part_settings->adbbackup)
@@ -3256,6 +3270,8 @@ bool TWPartition::Is_Sparse_Image(const string& Filename) {
 
 bool TWPartition::Flash_Sparse_Image(const string& Filename) {
 	string Command;
+
+	BlkDiscard();
 
 	gui_msg(Msg("flashing=Flashing {1}...")(Display_Name));
 
