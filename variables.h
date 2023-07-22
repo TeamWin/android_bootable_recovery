6ddb85cefaa8745f2de16af4d8b4d78a57e58ea2
Darth9 <reyprimerjefe@gmail.com>
2023-07-22 10:52:53 +0100

    partitionmanager: added force recovery fstab usage support
     * Device which doesn't want to use the additional fstab processing can set `ro.tw.addition_fstab=false`
       in there build prop.
    
    Change-Id: Ic498b82a2ff2a5461493ce4ad304f1a6679291df
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- variables.h
+++ variables.h
@@ -150,6 +150,9 @@
 #define TW_VIRTUAL_AB_ENABLED       "tw_virtual_ab.enabled"
 #define TW_AUTO_REFLASHTWRP_VAR     "tw_auto_reflashtwrp"
 
+
+// BUILD PROPS
+#define TW_READ_ADDITIONAL_FSTAB_PROP "ro.tw.addition_fstab"
 // Theme versioning
 // version 2 requires theme to handle power button as action togglebacklight
 // version 4 adds listbox support to reboot page
