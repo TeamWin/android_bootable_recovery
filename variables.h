3d5e3423ce739d151d82c8cdd9f44b846479a745
Mohd Faraz <androiabledroid@gmail.com>
2023-08-26 20:50:26 +0530

    kernel_module_loader: Load Modules as early as possible
     * This fixes unusually checking for unneeded directories for modules
    
     * Load Modules early, as it may required by decryption process
    
     * Tests: fastboot boot, ramdisk boot, fastbootd boot
    
    Change-Id: I2aae88b6dfc0c98867209d4e4eb7e9d503682199
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- variables.h
+++ variables.h
@@ -150,9 +150,11 @@
 #define TW_VIRTUAL_AB_ENABLED       "tw_virtual_ab.enabled"
 #define TW_AUTO_REFLASHTWRP_VAR     "tw_auto_reflashtwrp"
 
-
 // BUILD PROPS
 #define TW_READ_ADDITIONAL_FSTAB_PROP "ro.tw.addition_fstab"
+#define TW_FASTBOOT_MODE_PROP       "ro.twrp.fastbootd"
+#define TW_MODULES_MOUNTED_PROP     "twrp.modules.loaded"     // property for successfully mounted modules
+
 // Theme versioning
 // version 2 requires theme to handle power button as action togglebacklight
 // version 4 adds listbox support to reboot page
