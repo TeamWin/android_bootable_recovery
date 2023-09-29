34153d442e172d5e0873fd59d5b483ad4ed6f847
Mohd Faraz <androiabledroid@gmail.com>
2023-09-30 00:57:58 +0530

    partitionmanager: allow skipping additional fstab processing
    * Set `ro.tw.addition_fstab=false' in prop file or using PROPERTY_OVERRIDE
      to skip additional fstab processing
    * Set 'keymaster_ver' prop in device tree if you want to use a specific
      keymaster version regardless of what may be found in ramdisk manifest
      (if version can't be determined by checking device manifest)
    * Set above prop and add 'TW_FORCE_KEYMASTER_VER := true' if you want
      to enforce keymaster_ver set in device tree and skip checking on device
    
    In current state, if additional fstab cannot be found, then keymaster_ver never gets set.
    This breaks devices using twrp-common as the correct service will
    never start for decryption, resulting in a hang at the TWRP splash.
    
    Separating fstab processing allows keymaster to be found on device
    and keymaster_ver to be set regardless of fstab availability.
    
    If keymaster_ver cannot be determined by checking vendor on device,
    use default keymaster version set in device tree, or if not present,
    set from TWRP ramdisk based on binaries found, unless values are
    forced as mentioned above.
    
    Change-Id: Ic498b82a2ff2a5461493ce4ad304f1a6679291df
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -212,6 +212,7 @@ int TWPartitionManager::Process_Fstab(string Fstab_Filename, bool Display_Error,
 	FILE *fstabFile;
 	char fstab_line[MAX_FSTAB_LINE_LENGTH];
 	bool parse_userdata = false;
+	bool process_additional_fstab = android::base::GetBoolProperty(TW_READ_ADDITIONAL_FSTAB_PROP, true);
 	std::map<string, Flags_Map> twrp_flags;
 
 	fstabFile = fopen("/etc/twrp.flags", "rt");
@@ -356,33 +357,70 @@ clear:
 	if (recovery_mode && !parse_userdata) {
 		if (ven) ven->Mount(Display_Error);
 		if (odm) odm->Mount(Display_Error);
-		if (TWFunc::Find_Fstab(Fstab_Filename)) {
-			string service;
-			LOGINFO("Fstab: %s\n", Fstab_Filename.c_str());
-			TWFunc::copy_file(Fstab_Filename, additional_fstab, 0600, false);
-			Fstab_Filename = additional_fstab;
-			property_set("fstab.additional", "1");
-			TWFunc::Get_Service_From(ven, "keymaster", service);
-			LOGINFO("Keymaster version: '%s'\n", TWFunc::Get_Version_From_Service(service).c_str());
-			property_set("keymaster_ver", TWFunc::Get_Version_From_Service(service).c_str());
-			parse_userdata = true;
-			Reset_Prop_From_Partition("ro.crypto.dm_default_key.options_format.version", "", ven, odm);
-			Reset_Prop_From_Partition("ro.crypto.volume.metadata.method", "", ven, odm);
-			Reset_Prop_From_Partition("ro.crypto.volume.options", "", ven, odm);
-			Reset_Prop_From_Partition("external_storage.projid.enabled", "", ven, odm);
-			Reset_Prop_From_Partition("external_storage.casefold.enabled", "", ven, odm);
-			Reset_Prop_From_Partition("external_storage.sdcardfs.enabled", "", ven, odm);
 
-			if (ven) ven->UnMount(Display_Error);
-			if (odm) odm->UnMount(Display_Error);
-			goto parse;
+		// Reset the crypto volume props according to os.
+		Reset_Prop_From_Partition("ro.crypto.dm_default_key.options_format.version", "", ven, odm);
+		Reset_Prop_From_Partition("ro.crypto.volume.metadata.method", "", ven, odm);
+		Reset_Prop_From_Partition("ro.crypto.volume.options", "", ven, odm);
+		Reset_Prop_From_Partition("external_storage.projid.enabled", "", ven, odm);
+		Reset_Prop_From_Partition("external_storage.casefold.enabled", "", ven, odm);
+		Reset_Prop_From_Partition("external_storage.sdcardfs.enabled", "", ven, odm);
+		parse_userdata = true;
+
+		// Now Fetch the additional fstab only if required
+		if  (process_additional_fstab) {
+			if (TWFunc::Find_Fstab(Fstab_Filename)) {
+				LOGINFO("Fstab: %s\n", Fstab_Filename.c_str());
+				TWFunc::copy_file(Fstab_Filename, additional_fstab, 0600, false);
+				Fstab_Filename = additional_fstab;
+				property_set("fstab.additional", "1");
+				goto parse;
+			} else {
+				LOGINFO("Unable to parse vendor fstab\n");
+			}
 		} else {
-			if (ven) ven->UnMount(Display_Error);
-			if (odm) odm->UnMount(Display_Error);
-			LOGINFO("Unable to parse vendor fstab\n");
+			LOGINFO("Skipping Additional Fstab Processing\n");
+			property_set("fstab.additional", "0");
 		}
 	}
 	LOGINFO("Done processing fstab files\n");
+
+	// Fetch the Keymaster Version Service version to be started
+	std::string service;
+#ifndef TW_FORCE_KEYMASTER_VER
+	TWFunc::Get_Service_From("/vendor", "android.hardware.keymaster", service, true);
+
+	/* If we are unable to get the version from device vendor then
+		* set the version from the keymaster_ver prop if set
+		*/
+	if (service.empty()) {
+		// unmount partitions
+		if (ven) ven->UnMount(Display_Error);
+		if (odm) odm->UnMount(Display_Error);
+		// Use keymaster_ver prop set from device tree (if exists)
+		service = android::base::GetProperty(TW_KEYMASTER_VERSION_PROP, service);
+		if (service.empty()) {
+			LOGINFO("Keymaster_Ver::Unable to find vendor manifest on the device, and no default value set. Checking the ramdisk manifest\n");
+			TWFunc::Get_Service_From("/vendor", "android.hardware.keymaster", service, true);
+		} else {
+			LOGINFO("Keymaster_Ver::Unable to find vendor manifest on the device. Setting to default value.\n");
+		}
+	} else {
+		if (ven) ven->UnMount(Display_Error);
+		if (odm) odm->UnMount(Display_Error);
+	}
+#else
+	if (ven) ven->UnMount(Display_Error);
+	if (odm) odm->UnMount(Display_Error);
+	service = android::base::GetProperty(TW_KEYMASTER_VERSION_PROP, service);
+	if (service.empty()) {
+		LOGINFO("Keymaster_Ver::Force Keymaster_Ver flag found, but keymaster_ver prop not set.\n");
+	} else {
+		LOGINFO("Keymaster_Ver::Force Keymaster_Ver flag found.\n");
+	}
+#endif
+	LOGINFO("Keymaster_Ver::Using keymaster version '%s' for decryption\n", service.c_str());
+	android::base::SetProperty(TW_KEYMASTER_VERSION_PROP, service.c_str());
 
 	return true;
 }
