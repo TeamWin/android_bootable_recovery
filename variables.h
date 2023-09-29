bf54307844defc5d9d64ee8087f7a519b688529c
Mohd Faraz <androiabledroid@gmail.com>
2023-09-30 00:57:58 +0530

    partitionmanager: allow skipping additional fstab processing
    * Set `ro.tw.addition_fstab=false' in prop file or using PROPERTY_OVERRIDE
      to skip additional fstab processing
    * Set 'keymaster_ver' prop in device tree if you want to use a specific
      keymaster version regardless of what may be found in ramdisk manifest
      (if version can't be determined by checking device manifest)
    * Set above prop and add 'TW_FORCE_KEYMASTER_VER := true' if you want
      to enforce keymaster_ver set in device tree and skip checking on device
    
    In current state, if additional fstab cannot be found, then keymaster_ver never gets set.
    This breaks devices using twrp-common as the correct service will
    never start for decryption, resulting in a hang at the TWRP splash.
    
    Separating fstab processing allows keymaster to be found on device
    and keymaster_ver to be set regardless of fstab availability.
    
    If keymaster_ver cannot be determined by checking vendor on device,
    use default keymaster version set in device tree, or if not present,
    set from TWRP ramdisk based on binaries found, unless values are
    forced as mentioned above.
    
    Change-Id: Ic498b82a2ff2a5461493ce4ad304f1a6679291df
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- variables.h
+++ variables.h
@@ -153,6 +153,8 @@
 // BUILD PROPS
 #define TW_FASTBOOT_MODE_PROP         "ro.twrp.fastbootd"
 #define TW_MODULES_MOUNTED_PROP       "twrp.modules.loaded"     // property for successfully mounted modules
+#define TW_READ_ADDITIONAL_FSTAB_PROP "ro.tw.addition_fstab"
+#define TW_KEYMASTER_VERSION_PROP     "keymaster_ver"
 
 // Theme versioning
 // version 2 requires theme to handle power button as action togglebacklight
