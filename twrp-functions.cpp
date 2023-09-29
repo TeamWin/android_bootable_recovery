ad23fff166c1443c9d87a38c96fe26c33506fdd5
Mohd Faraz <androiabledroid@gmail.com>
2023-09-30 00:57:58 +0530

    partitionmanager: added force recovery fstab usage support
     * Device which doesn't want to use the additional fstab processing can set `ro.tw.addition_fstab=false`
       in there build prop.
    
    Change-Id: Ic498b82a2ff2a5461493ce4ad304f1a6679291df
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- twrp-functions.cpp
+++ twrp-functions.cpp
@@ -57,6 +57,10 @@
 #include "cutils/properties.h"
 #include "cutils/android_reboot.h"
 #include <sys/reboot.h>
+#ifdef TW_INCLUDE_CRYPTO
+#include "gui/rapidxml.hpp"
+#include "gui/pages.hpp"
+#endif
 #endif // ndef BUILD_TWRPTAR_MAIN
 #ifndef TW_EXCLUDE_ENCRYPTED_BACKUPS
 	#include "openaes/inc/oaes_lib.h"
@@ -1584,55 +1588,93 @@ bool TWFunc::Find_Fstab(string &fstab) {
 	return true;
 }
 
-bool TWFunc::Get_Service_From(TWPartition *Partition, std::string Service, std::string &Res) {
-	Partition->Mount(true);
-	std::string Path = Partition->Get_Mount_Point() + "/etc/init/";
-	std::string Name;
-	std::vector<std::string> Data;
-	bool Found = false, ret = false;
-	DIR* dir;
-	struct dirent* der;
-	dir = opendir(Path.c_str());
-	while ((der = readdir(dir)) != NULL)
-	{
-		Name = der->d_name;
-		if (Name.find(Service) != string::npos) {
-			Found = true;
-			Path += Name;
-			break;
+static inline std::string Get_Version_From_FQ(std::string name) {
+	int start, end;
+	start = name.find('@') + 1;
+	end = name.find(":") - start;
+	return name.substr(start, end);
+}
+
+bool TWFunc::Get_Service_From(std::string partition, std::string service, std::string &res) {
+	LOGINFO("Looking for service %s in manifest\n", service.c_str());
+	TWPartition* part = PartitionManager.Find_Partition_By_Path(partition);
+	partition = partition + "/etc/vintf/manifest";
+	std::string path, platform = android::base::GetProperty("ro.board.platform", "");
+	bool mounted = false, ret = false;
+
+	if (part) {
+		mounted = true;
+		part->Mount(true);
+	}
+
+	// Let's find the service xml if exists;
+	Exec_Cmd("find " + partition + "/ -type f -name *" + service + "*", path, false);
+	if (path.empty()) {
+		LOGINFO("Separate manifest doesn't exist for %s\n", service.c_str());
+		// Look for main manifest*.xml
+		path = partition + "_" + platform + ".xml";
+
+		// Goto legacy manifest if platform manifest is not found.
+		if (!Path_Exists(path)) {
+			path = partition + ".xml";
 		}
 	}
-	closedir(dir);
 
-	if (!Found) {
-		LOGINFO("Unable to locate service RC\n");
-		goto finish;
-	}
-
-	if (read_file(Path, Data) != 0) {
-		LOGINFO("Unable to read file '%s'\n", Path.c_str());
-		goto finish;
-	}
-
-	for (int index = 0; index < Data.size(); index++) {
-		Name = Data.at(index);
-		if (Name.find("service") != string::npos) {
-			Res = Name.substr(Name.find_last_of('/')+1);
-			ret = true;
-			goto finish;
+process:
+	LOGINFO("Using manifest path: %s\n", path.c_str());
+	char* manifest = PageManager::LoadFileToBuffer(path, NULL);
+	if (manifest == NULL && !mounted) {
+		LOGINFO("Unable to find the manifest in the ramdisk, mounting the partition\n");
+		if (part) {
+			part->Mount(true);
+			mounted = !mounted;
+			goto process;
+		}
+	} else {
+		xml_document<>* vintfManifest = new xml_document<>();
+		vintfManifest->parse<0>(manifest);
+		xml_node<>* manifestNode = vintfManifest->first_node("manifest");
+		std::string version;
+		if (manifestNode) {
+			for (xml_node<>* child = manifestNode->first_node(); child; child = child->next_sibling()) {
+				std::string type = child->name();
+				if (type == "hal") {
+					xml_node<>* nameNode = child->first_node("name");
+					type = nameNode->value();
+					if (type == service) {
+						xml_node<> *versionNode = child->first_node("version");
+						if (versionNode != nullptr) {
+							LOGINFO("Found version in manifest: %s\n", versionNode->value());
+						} else {
+							versionNode = child->first_node("fqname");
+							if (versionNode == nullptr) goto finish;
+								LOGINFO("Found fqname in manifest: %s\n", versionNode->value());
+						}
+						version = versionNode->value();
+						if (version.find('@') == std::string::npos) {
+							res = version;
+						} else {
+							res = Get_Version_From_FQ(version);
+						}
+						LOGINFO("Keymaster version from manifest: '%s'\n", res.c_str());
+						ret = true;
+					}
+				}
+			}
 		}
 	}
 
 finish:
-	Partition->UnMount(true);
+	if (mounted) {
+		part->UnMount(true);
+		mounted = !mounted;
+	}
+	if (!ret) {
+		// Go for TWRP Ramdisk manifest in the end.
+		path = "/vendor/etc/vintf/manifest.xml";
+		goto process;
+	}
 	return ret;
-}
-
-std::string TWFunc::Get_Version_From_Service(std::string name) {
-	int start, end;
-	start = name.find('@') + 1;
-	end = name.find("-") - start;
-	return name.substr(start, end);
 }
 
 #endif // ndef BUILD_TWRPTAR_MAIN
