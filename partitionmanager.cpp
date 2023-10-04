6602b2d00bb8803d4f6f4ffbc677b90588480379
Captain Throwback <captainthrowback@hotmail.com>
2023-10-04 10:01:17 -0400

    format data: run factoryreset.sh after complete
    Since we run factoryreset.sh after a Factory Reset,
    it stands to reason that we should also run it
    after a Format Data.
    
    Change-Id: Ic2ed30e3064308e39381746c88c98b1a3eb64439

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -1714,6 +1714,7 @@ int TWPartitionManager::Format_Data(void) {
 		gui_msg(Msg(msg::kError, "unable_to_locate=Unable to locate {1}.")("/data"));
 		return false;
 	}
+	TWFunc::check_and_run_script("/system/bin/factoryreset.sh", "Factory Reset Script");
 	return false;
 }
 
