sekaiacg/twrp_recovery

commit 25062d7904430cdb83e4b4eaf6db38715da88e49

Set data/recovery for Settings: complementary code
After https://gerrit.twrp.me/c/android_bootable_recovery/+/5670 some functions were not working properly, such as change TWRP folder and custom themes.

Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>
Change-Id: Ifaa777e528a6ca68e628d1719ba6d232851d338d

partitionmanager.cpp
@@ -3105,6 +3105,8 @@ bool TWPartitionManager::Decrypt_Adopted() {
 		}
 	}
 
+	//Devices without encryption do not run the Post_Decrypt function so the "data/recovery" folder was not being created on these devices
+	DataManager::SetValue("tw_settings_path", TW_STORAGE_PATH);
 	LOGINFO("Decrypt adopted storage starting\n");
 	char* xmlFile = PageManager::LoadFileToBuffer(path, NULL);
 	xml_document<> *doc = NULL;

twrp-functions.cpp
@@ -1449,7 +1449,7 @@ string TWFunc::Check_For_TwrpFolder() {
 			type = Get_D_Type_From_Stat(fullPath);
 		}
 
-		if (type == DT_DIR && Path_Exists(fullPath + '/' + TW_SETTINGS_FILE)) {
+		if (type == DT_DIR && Path_Exists(fullPath + "/.twrpcf")) {
 			if ('/' + name == TW_DEFAULT_RECOVERY_FOLDER) {
 				oldFolder = name;
 			} else {
@@ -1462,6 +1462,9 @@ string TWFunc::Check_For_TwrpFolder() {
 
 	if (oldFolder == "" && customTWRPFolders.empty()) {
 		LOGINFO("No recovery folder found. Using default folder.\n");
+		//Creates the TWRP folder if it does not exist on the device and if the folder has not been changed to a new name
+		mainPath += TW_DEFAULT_RECOVERY_FOLDER;
+		mkdir(mainPath.c_str(), 0777);
 		goto exit;
 	} else if (customTWRPFolders.empty()) {
 		LOGINFO("No custom recovery folder found. Using TWRP as default.\n");

gui/action.cpp
@@ -34,6 +34,7 @@
 #include <dirent.h>
 #include <private/android_filesystem_config.h>
 #include <android-base/properties.h>
+#include <fstream>
 
 #include <string>
 #include <sstream>
@@ -2347,6 +2348,10 @@ int GUIAction::applycustomtwrpfolder(string arg __unused)
 	if (ret) {
 		DataManager::SetValue(TW_RECOVERY_FOLDER_VAR, '/' + arg);
 		DataManager::SetValue(TW_BACKUPS_FOLDER_VAR, newBackupFolder);
+		//Creates an empty file that marks which folder is TWRP with the renamed new name, after reboot.
+		string path= newFolder + "/.twrpcf";
+		std::ofstream twrpcf(path);
+		twrpcf.close();
 	}
 	operation_end((int)!ret);
 	return 0;

gui/gui.cpp
@@ -799,7 +799,7 @@ extern "C" int gui_loadResources(void)
 	{
 		std::string theme_path;
 
-		theme_path = DataManager::GetSettingsStoragePath();
+		theme_path = DataManager::GetCurrentStoragePath();
 		if (!PartitionManager.Mount_Settings_Storage(false))
 		{
 			int retry_count = 5;
@@ -849,7 +849,7 @@ extern "C" int gui_loadCustomResources(void)
 		return -1;
 	}
 
-	std::string theme_path = DataManager::GetSettingsStoragePath();
+	std::string theme_path = DataManager::GetCurrentStoragePath();
 	theme_path += TWFunc::Check_For_TwrpFolder() + "/theme/ui.zip";
 	// Check for a custom theme
 	if (TWFunc::Path_Exists(theme_path)) {

gui/pages.cpp
@@ -1505,7 +1505,7 @@ int PageManager::RunReload() {
 		return 0;
 
 	mReloadTheme = false;
-	theme_path = DataManager::GetSettingsStoragePath();
+	theme_path = DataManager::GetCurrentStoragePath();
 	if (PartitionManager.Mount_By_Path(theme_path.c_str(), 1) < 0) {
 		LOGERR("Unable to mount %s during gui_reload_theme function.\n", theme_path.c_str());
 		ret_val = 1;

TeamWin/android_bootable_recovery

commit c0c65ee767d1628dbb052ad286f19a1f5f8fae24

set data/recovery on devices where the TW_INCLUDE_CRYPTO flag has not been set
On some devices where the cryptography is broken, the maintainers do not set the flag and the data/recovery is not being created, so that at each boot the settings are not saved.

This commit also fixes the mounting of the external SDcard caused by the previous patch: https://github.com/TeamWin/android_bootable_recovery/commit/25062d7904430cdb83e4b4eaf6db38715da88e49#commitcomment-133147543
Change-Id: I4b308d17aea18bdda215b1f11393e40f2d4647b9
Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>

data.cpp
@@ -1140,5 +1140,5 @@ void DataManager::Vibrate(const string& varName)
 void DataManager::LoadTWRPFolderInfo(void)
 {
 	SetValue(TW_RECOVERY_FOLDER_VAR, TWFunc::Check_For_TwrpFolder());
-	mBackingFile = GetSettingsStoragePath() + GetStrValue(TW_RECOVERY_NAME) + '/' + TW_SETTINGS_FILE;
+	mBackingFile = GetSettingsStoragePath() + GetStrValue(TW_RECOVERY_NAME) + TW_SETTINGS_FILE;
 }

partition.cpp
@@ -1275,6 +1275,7 @@ void TWPartition::Setup_Data_Media() {
 			Storage_Path = Mount_Point + "/media/0";
 			Symlink_Path = Storage_Path;
 			DataManager::SetValue(TW_INTERNAL_PATH, Mount_Point + "/media/0");
+			DataManager::SetValue("tw_settings_path", TW_STORAGE_PATH);
 			UnMount(true);
 		}
 		DataManager::SetValue("tw_has_internal", 1);

partitionmanager.cpp
@@ -3169,8 +3169,6 @@ bool TWPartitionManager::Decrypt_Adopted() {
 		}
 	}
 
-	//Devices without encryption do not run the Post_Decrypt function so the "data/recovery" folder was not being created on these devices
-	DataManager::SetValue("tw_settings_path", TW_STORAGE_PATH);
 	LOGINFO("Decrypt adopted storage starting\n");
 	char* xmlFile = PageManager::LoadFileToBuffer(path, NULL);
 	xml_document<> *doc = NULL;

twrp-functions.cpp
@@ -1452,9 +1452,11 @@ string TWFunc::Check_For_TwrpFolder() {
 
 	if (oldFolder == "" && customTWRPFolders.empty()) {
 		LOGINFO("No recovery folder found. Using default folder.\n");
-		//Creates the TWRP folder if it does not exist on the device and if the folder has not been changed to a new name
-		mainPath += TW_DEFAULT_RECOVERY_FOLDER;
-		mkdir(mainPath.c_str(), 0777);
+		TWPartition* SDCard = PartitionManager.Find_Partition_By_Path(DataManager::GetCurrentStoragePath());
+		if (SDCard->Mount(true)) {
+			mainPath += TW_DEFAULT_RECOVERY_FOLDER;
+			mkdir(mainPath.c_str(), 0777);
+		}
 		goto exit;
 	} else if (customTWRPFolders.empty()) {
 		LOGINFO("No custom recovery folder found. Using TWRP as default.\n");