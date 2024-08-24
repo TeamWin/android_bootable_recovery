d3b048cd3730191e62d287bcd2fdb385b9ebd280
bigbiff <bigbiff@teamw.in>
2024-08-24 16:23:35 -0400

    tasks: move task_profiles.json to device tree
    Change-Id: I74a6f7c8e9c2cdfa68a04776370c98d728dd7a3f

--- Android.mk
+++ Android.mk
@@ -68,7 +68,9 @@ LOCAL_STATIC_LIBRARIES += libavb \
                           libsnapshot_cow \
                           libsnapshot_nobinder \
                           libtwrpinstall \
-                          update_metadata-protos
+                          update_metadata-protos \
+                          android.hardware.health-translate-ndk \
+                          android.hardware.health-V2-ndk
 
 LOCAL_SHARED_LIBRARIES += android.hardware.boot@1.0 \
                           android.hardware.boot@1.1 \
@@ -550,7 +552,6 @@ TWRP_REQUIRED_MODULES += \
 ifneq ($(TW_INCLUDE_CRYPTO),)
 TWRP_REQUIRED_MODULES += \
     vold_prepare_subdirs \
-    task_recovery_profiles.json \
     fscryptpolicyget.recovery \
     keystore_auth \
     keystore2 \
