37b16048250b57783a234ab438bee2c277954898
mrfluffyoven <mrfluffyoven@gmail.com>
2026-06-26 10:55:50 +0000

    recovery_ui: Add TSP touch fix on screen wake for Samsung incell panels
    this is a fix for when samsung incell touchscreens sleep when the screen
    times out, then when you wake the screen back up the touch is dead, this
    change runs "fw_update" to check if the screen is active and when it
    is it wakes the touch screen up again
    
    Change-Id: I0f52135c9ae243b2034a692f09f2887556abc879

--- recovery_ui/ui.cpp
+++ recovery_ui/ui.cpp
@@ -419,6 +419,22 @@ void RecoveryUI::EnqueueKey(int key_code) {
   }
 }
 
+static void SamsungTSPTouchFix() {
+  const char* tsp_cmd = "/sys/class/sec/tsp/cmd";
+  const char* tsp_result = "/sys/class/sec/tsp/cmd_result";
+  if (access(tsp_cmd, W_OK) != 0) return;
+
+  android::base::WriteStringToFile("fw_update", tsp_cmd);
+  std::string result;
+  android::base::ReadFileToString(tsp_result, &result);
+
+  if (result.find("OK") == std::string::npos) {
+    android::base::WriteStringToFile("incell_power_control,0", tsp_cmd);
+    android::base::WriteStringToFile("incell_power_control,1", tsp_cmd);
+    android::base::WriteStringToFile("fw_update", tsp_cmd);
+  }
+}
+
 void RecoveryUI::SetScreensaverState(ScreensaverState state) {
   switch (state) {
     case ScreensaverState::NORMAL:
@@ -427,6 +443,7 @@ void RecoveryUI::SetScreensaverState(ScreensaverState state) {
         screensaver_state_ = ScreensaverState::NORMAL;
         LOG(INFO) << "Brightness: " << brightness_normal_value_ << " (" << brightness_normal_
                   << "%)";
+        SamsungTSPTouchFix();
       } else {
         LOG(WARNING) << "Unable to set brightness to normal";
       }
